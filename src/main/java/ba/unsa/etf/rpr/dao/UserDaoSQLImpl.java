5981c4ceb01e57adc3e603faab623bcccf3bce8b
tselimovic2 <tselimovic2@etf.unsa.ba>
2022-12-15 14:13:24 +0100

    Implemented UserDaoSQLImpl class

---
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
@@ -0,0 +1,122 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.User;
+
+import java.io.FileReader;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class UserDaoSQLImpl implements UserDao{
+    Connection connection;
+    public UserDaoSQLImpl(){
+        try {
+            FileReader reader = new FileReader("src//main//resources//db.properties");
+            Properties p = new Properties();
+            p.load(reader);
+            String s1=p.getProperty("user");
+            String s2=p.getProperty("password");
+            String s3=p.getProperty("url");
+            this.connection = DriverManager.getConnection(s3, s1, s2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public User getById(int id) {
+        String query = "SELECT * FROM Users WHERE user_id = ?";
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()){ // result set is iterator.
+                User user = new User();
+                user.setUser_id(rs.getInt("user_id"));
+                user.setName(rs.getString("name"));
+                user.setRole(rs.getBoolean("role"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                rs.close();
+                return user;
+            }else{
+                return null; // if there is no elements in the result set return null
+            }
+        }catch (SQLException e){
+            e.printStackTrace(); // poor error handling
+        }
+        return null;
+    }
+    @Override
+    public User add(User item) {
+        String insert = "INSERT INTO Users(name,role,username,password) VALUES(?,?,?,?)";
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, item.getName());
+            stmt.setBoolean(2, item.isRole());
+            stmt.setString(3, item.getUsername());
+            stmt.setString(4, item.getPassword());
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next(); // we know that there is one key
+            item.setUser_id(rs.getInt(1)); //set id to return it back
+            return item;
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public User update(User item) {
+        String insert = "UPDATE Users SET name = ?, role = ?, username = ?, password = ? WHERE user_id = ? ";
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+            stmt.setObject(1, item.getName());
+            stmt.setObject(2, item.isRole());
+            stmt.setObject(3, item.getUsername());
+            stmt.setObject(4, item.getPassword());
+            stmt.setObject(5, item.getUser_id());
+            stmt.executeUpdate();
+            return item;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    @Override
+    public void delete(int id) {
+        String insert = "DELETE FROM Users WHERE user_id = ?";
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+            stmt.setObject(1, id);
+            stmt.executeUpdate();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+        String query = "SELECT * FROM users";
+        List<User> users= new ArrayList<>();
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){ // result set is iterator.
+                User user = new User();
+                user.setUser_id(rs.getInt("user_id"));
+                user.setName(rs.getString("name"));
+                user.setRole(rs.getBoolean("role"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                users.add(user);
+            }
+            rs.close();
+        }catch (SQLException e){
+            e.printStackTrace(); // poor error handling
+        }
+        return users;
+    }
+}
