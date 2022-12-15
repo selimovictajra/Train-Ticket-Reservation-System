8fca22f2deba2fad9b65727bc627533b4ab2b9fb
tselimovic2 <tselimovic2@etf.unsa.ba>
2022-12-15 12:23:46 +0100

    Implemented TrainDao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/TrainDao.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Train;
+
+/**
+ * Dao interface for Train domain bean
+ *
+ * @author Tajra Selimovic
+ */
+public interface TrainDao extends Dao<Train> {
+}
