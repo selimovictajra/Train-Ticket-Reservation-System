0ab12278921b22df7febd6e5e28ff576bca7e523
tselimovic2 <tselimovic2@etf.unsa.ba>
2022-12-17 21:21:31 +0100

    Created CancelButtonOnAction method in LoginController class

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,6 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
-public class LoginController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+public class LoginController {
+    @FXML
+    private Button cancelButton;
+
+    public void CancelButtonOnAction(ActionEvent ae) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
 }
 
