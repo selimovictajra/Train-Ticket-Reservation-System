d05fc7b08ae8434c133b47d524a41a1378510235
tselimovic2 <tselimovic2@etf.unsa.ba>
2022-12-18 17:15:04 +0100

    Added closeButtonOnAction method in RegisterController

--- src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
@@ -1,4 +1,16 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class RegisterController {
+    @FXML
+    private Button closeButton;
+
+    public void closeButtonOnAction(ActionEvent ae) {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
 }
