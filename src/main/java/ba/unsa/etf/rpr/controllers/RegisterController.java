8c6bb1d43cff9c18ddba67ece46e43c56af70488
tselimovic2 <tselimovic2@etf.unsa.ba>
2022-12-18 19:41:50 +0100

    Small fix in closeButtonOnAction method

--- src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
@@ -1,16 +1,43 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.UserDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.User;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.sql.*;
 
 public class RegisterController {
     @FXML
     private Button closeButton;
+    @FXML
+    private TextField fullnameTextFiled;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private PasswordField conpassField;
+    @FXML
+    private Label messageLabel1;
+    @FXML
+    private Label messageLabel2;
+    @FXML
+    private Label messageLabel3;
+    @FXML
+    private Label messageLabel4;
+
 
     public void closeButtonOnAction(ActionEvent ae) {
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
+        Platform.exit();
     }
+
 }
