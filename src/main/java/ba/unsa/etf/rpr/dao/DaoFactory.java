a2472bf17e7319dea6104b1844f9adfaca705a2a
tselimovic2 <tselimovic2@etf.unsa.ba>
2022-12-26 13:13:13 +0100

    Refactoring DaoFactory

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -2,11 +2,16 @@ package ba.unsa.etf.rpr.dao;
 
 public class DaoFactory {
     private static final TrainDao trainDao = new TrainDaoSQLImpl();
+    private static final UserDao userDao = new UserDaoSQLImpl();
 
     public DaoFactory() {
     }
 
     public static TrainDao trainDao() {
         return trainDao;
+    }
+
+    public static UserDao userDao() {
+        return userDao;
     }
 }
