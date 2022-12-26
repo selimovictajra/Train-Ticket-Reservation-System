ad0b7d1d04a282bb7f81df4f47bd6b03044c43a6
tselimovic2 <tselimovic2@etf.unsa.ba>
2022-12-26 14:15:18 +0100

    Small fix in RegisterController

--- src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegisterController.java
@@ -54,7 +54,7 @@ public class RegisterController {
             messageLabel4.setText("");
         }
         else if (!(passwordField.getText().equals(conpassField.getText()))) {
-            messageLabel3.setText("kikiriki.");
+            messageLabel3.setText("Password does not match.");
             messageLabel1.setText("");
             messageLabel2.setText("");
             messageLabel4.setText("");
