6bcf9dcec60fc3c3ede0436c1340a8505749887a
tselimovic2 <tselimovic2@etf.unsa.ba>
2022-12-26 12:40:08 +0100

    Added constructors in TrainException

--- src/main/java/ba/unsa/etf/rpr/exceptions/TrainException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/TrainException.java
@@ -1,5 +1,11 @@
 package ba.unsa.etf.rpr.exceptions;
 
 public class TrainException extends Exception{
+    public TrainException(String message) {
+        super(message);
+    }
 
+    public TrainException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
