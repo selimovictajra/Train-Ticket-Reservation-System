74f288686555fa88ae4ccb548b33abc125c17e00
tselimovic2 <tselimovic2@etf.unsa.ba>
2022-12-26 12:43:31 +0100

    Added getter and setter for connection in AbstractDao

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -2,5 +2,16 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Idable;
 
+import java.sql.*;
+import java.util.*;
+
 public abstract class AbstractDao<T extends Idable> implements Dao<T>{
+    private Connection connection;
+    private String tableName;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+
 }
