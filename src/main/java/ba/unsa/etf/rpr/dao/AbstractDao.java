5f7218c6bd3477ab4acfeb5a249eda5be9fe29e9
tselimovic2 <tselimovic2@etf.unsa.ba>
2022-12-26 12:50:42 +0100

    Added constructor in AbstractDao

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -9,6 +9,21 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T>{
     private Connection connection;
     private String tableName;
 
+    public AbstractDao(String tableName) {
+        try{
+            this.tableName = tableName;
+            Properties p = new Properties();
+            p.load(ClassLoader.getSystemResource("application.properties").openStream());
+            String url = p.getProperty("db.url");
+            String username = p.getProperty("db.user");
+            String password = p.getProperty("db.password");
+            this.connection = DriverManager.getConnection(url, username, password);
+        }catch (Exception e){
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
     public Connection getConnection() {
         return connection;
     }
