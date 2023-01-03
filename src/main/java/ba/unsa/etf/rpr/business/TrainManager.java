1dbe73a58fc5e0b87c497179d4e42227f6b160bf
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-03 13:48:13 +0100

    Added delete method

--- src/main/java/ba/unsa/etf/rpr/business/TrainManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/TrainManager.java
@@ -9,4 +9,16 @@ public class TrainManager {
     public Train update(Train cat) throws TrainException {
         return DaoFactory.trainDao().update(cat);
     }
+
+    public void delete(int movieId) throws TrainException{
+        try{
+            DaoFactory.trainDao().delete(movieId);
+        }
+        catch (TrainException e){
+            if (e.getMessage().contains("FOREIGN KEY")){
+                throw new TrainException("Cannot delete this train since tickets have already been reserved.");
+            }
+            throw e;
+        }
+    }
 }
