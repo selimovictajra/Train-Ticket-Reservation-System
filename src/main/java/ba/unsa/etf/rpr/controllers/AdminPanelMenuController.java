5546936efc00e39ec9113a2e43bcab7be12f37c2
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-03 18:05:57 +0100

    Added refreshTrains method

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelMenuController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelMenuController.java
@@ -1,13 +1,52 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.ReservationManager;
+import ba.unsa.etf.rpr.business.TrainManager;
+import ba.unsa.etf.rpr.business.UserManager;
+import ba.unsa.etf.rpr.domain.Train;
+import ba.unsa.etf.rpr.exceptions.TrainException;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
+import java.time.LocalDate;
 import java.util.Objects;
 
 public class AdminPanelMenuController {
+    private final TrainManager trainManager = new TrainManager();
+    private final UserManager userManager = new UserManager();
+    private final ReservationManager reservationManager = new ReservationManager();
+
+    @FXML
+    BorderPane adminPane;
+
+    @FXML
+    public TableView<Train> trainTable;
+    @FXML
+    public TableColumn<Train, String> trainIdColumn;
+    @FXML
+    public TableColumn<Train, String> routeColumn;
+    @FXML
+    public TableColumn<Train, LocalDate> departureColumn;
+    @FXML
+    public TableColumn<Train, String> capacityColumn;
+
+    void refreshTrains() {
+        try {
+            trainTable.setItems(FXCollections.observableList(trainManager.getAll()));
+        } catch (TrainException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void homeLinkOnAction(javafx.event.ActionEvent actionEvent) {
         try {
