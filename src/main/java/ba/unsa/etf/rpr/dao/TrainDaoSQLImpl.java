d6cceea45de237dd390d6cbbbf13f64885adb4c6
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-03 20:02:19 +0100

    Small changes

--- src/main/java/ba/unsa/etf/rpr/dao/TrainDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/TrainDaoSQLImpl.java
@@ -17,7 +17,7 @@ public class TrainDaoSQLImpl extends AbstractDao<Train> implements TrainDao{
     public Train row2object(ResultSet rs) throws TrainException {
         try {
             Train movie = new Train();
-            movie.setId(rs.getInt("train_id"));
+            movie.setId(rs.getInt("id"));
             movie.setRoute(rs.getString("route"));
             movie.setDeparture(Timestamp.valueOf(rs.getTimestamp("departure").toLocalDateTime()));
             movie.setCapacity(rs.getInt("capacity"));
@@ -30,7 +30,7 @@ public class TrainDaoSQLImpl extends AbstractDao<Train> implements TrainDao{
     @Override
     public Map<String, Object> object2row(Train object) {
         Map<String, Object> item = new TreeMap<>();
-        item.put("train_id", object.getId());
+        item.put("id", object.getId());
         item.put("route", object.getRoute());
         item.put("departure", object.getDeparture());
         item.put("capacity", object.getCapacity());
