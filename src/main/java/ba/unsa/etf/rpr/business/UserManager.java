9283d4ee5dbfa9d80ff1bf9f830f67195aac2770
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-03 13:54:33 +0100

    Created UserManager class

---
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.exceptions.TrainException;
+
+public class UserManager {
+
+    public void delete(int categoryId) throws TrainException {
+        try{
+            DaoFactory.userDao().delete(categoryId);
+        }
+        catch (TrainException e){
+            if (e.getMessage().contains("FOREIGN KEY")){
+                throw new TrainException("NO");
+            }
+            throw e;
+        }
+
+    }
+}
