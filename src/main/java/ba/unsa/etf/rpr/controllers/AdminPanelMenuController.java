9176a1d89e89846e91b4d99feb24ece065430574
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-03 13:30:41 +0100

    Added logoutLinkOnAction method

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelMenuController.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class AdminPanelMenuController {
+    public void logoutLinkOnAction(javafx.event.ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
+            Stage stage = (Stage)((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception exception) {
+            exception.printStackTrace();
+            exception.getCause();
+        }
+    }
+}
