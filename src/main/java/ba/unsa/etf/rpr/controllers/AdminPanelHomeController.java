6acfb6b65b36f77418bd23a9403c949ed3649aa6
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-03 13:01:19 +0100

    Added menuLinkOnAction method

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelHomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelHomeController.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.UserDaoSQLImpl;
+import ba.unsa.etf.rpr.exceptions.TrainException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.sql.SQLException;
+import java.util.Objects;
+
 public class AdminPanelHomeController {
+    private Label usersLabel;
+
+    public void menuLinkOnAction(javafx.event.ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/adminpanelmenu.fxml")));
+            Stage stage = (Stage)((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception exception) {
+            exception.printStackTrace();
+            exception.getCause();
+        }
+    }
 }
