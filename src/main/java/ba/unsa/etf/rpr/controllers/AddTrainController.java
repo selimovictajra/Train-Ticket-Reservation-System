8d277564c3bc644c41e9fd0f43dc7d421b6e9a6f
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-13 16:35:05 +0100

    Added homeLinkOnAction method

--- src/main/java/ba/unsa/etf/rpr/controllers/AddTrainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AddTrainController.java
@@ -35,6 +35,20 @@ public class AddTrainController {
         minBox.getItems().addAll(min);
     }
 
+    public void homeLinkOnAction(javafx.event.ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/adminpanelhome.fxml")));
+            Stage stage = (Stage) ((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
     public void logoutLinkOnAction(javafx.event.ActionEvent actionEvent) {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
