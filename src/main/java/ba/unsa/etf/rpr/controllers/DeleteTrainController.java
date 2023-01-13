79b55fc366a07a91eed6e8d8ee7f66b499cc2e63
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-13 14:25:45 +0100

    Added initialize

--- src/main/java/ba/unsa/etf/rpr/controllers/DeleteTrainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/DeleteTrainController.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.business.TrainManager;
+import ba.unsa.etf.rpr.domain.Train;
+import ba.unsa.etf.rpr.exceptions.TrainException;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
@@ -16,5 +18,13 @@ public class DeleteTrainController {
     private TrainManager trainManager = new TrainManager();
     private Label message;
 
-
+    @FXML
+    public void initialize() throws TrainException {
+        List<Train> list = trainManager.getAll();
+        for (int i = 0; i < list.size(); i++) {
+            Train train = list.get(i);
+            trainIds.add(train.getId());
+        }
+        trainRoutes.getItems().addAll(trainIds);
+    }
 }
