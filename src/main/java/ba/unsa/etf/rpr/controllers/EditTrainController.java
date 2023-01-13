9c7efda52ca17804e919a3664542ac2869b6a4eb
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-13 20:41:47 +0100

    Added homeLinkOnAction method

--- src/main/java/ba/unsa/etf/rpr/controllers/EditTrainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EditTrainController.java
@@ -2,14 +2,20 @@ package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.business.TrainManager;
 import ba.unsa.etf.rpr.domain.Train;
+import ba.unsa.etf.rpr.exceptions.TrainException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class EditTrainController {
     @FXML
@@ -42,6 +48,20 @@ public class EditTrainController {
             routeBox.getItems().addAll(trainIds);
             hourBox.getItems().addAll(hour);
             minBox.getItems().addAll(min);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    public void homeLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/adminpanelhome.fxml")));
+            Stage stage = (Stage) ((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
         }
         catch (Exception e) {
             e.printStackTrace();
