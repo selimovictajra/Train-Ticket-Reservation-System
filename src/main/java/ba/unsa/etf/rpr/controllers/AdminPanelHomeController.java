63f14b7dc3c513a3f7e1e890904892e6a6868c3e
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-13 17:42:10 +0100

    Added addTrainLinkOnAction method

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelHomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelHomeController.java
@@ -54,6 +54,19 @@ public class AdminPanelHomeController {
             exception.getCause();
         }
     }
+    public void addTrainLinkOnAction(javafx.event.ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/addLink.fxml")));
+            Stage stage = (Stage)((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception exception) {
+            exception.printStackTrace();
+            exception.getCause();
+        }
+    }
     public void logoutLinkOnAction(javafx.event.ActionEvent actionEvent) {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
