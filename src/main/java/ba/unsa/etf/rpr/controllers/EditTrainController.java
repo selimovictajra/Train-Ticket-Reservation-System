0d52105245c5ba83e4fc5dcf4a1bccdaa365532d
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-13 20:43:38 +0100

    Added addTrainLinkOnAction method

--- src/main/java/ba/unsa/etf/rpr/controllers/EditTrainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EditTrainController.java
@@ -94,4 +94,17 @@ public class EditTrainController {
             e.getCause();
         }
     }
+    public void addTrainLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/addTrain.fxml")));
+            Stage stage = (Stage) ((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
 }
