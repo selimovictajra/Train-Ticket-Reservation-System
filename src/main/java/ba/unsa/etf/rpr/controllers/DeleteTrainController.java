4eb81a86e7da8d1351b42d1c1021e7a8e4230dbb
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-13 20:19:08 +0100

    Added editLinkOnAction method

--- src/main/java/ba/unsa/etf/rpr/controllers/DeleteTrainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/DeleteTrainController.java
@@ -90,7 +90,19 @@ public class DeleteTrainController {
             exception.getCause();
         }
     }
-
+    public void editTrainLinkOnAction(javafx.event.ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/editLink.fxml")));
+            Stage stage = (Stage)((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception exception) {
+            exception.printStackTrace();
+            exception.getCause();
+        }
+    }
     public void logoutLinkOnAction(javafx.event.ActionEvent actionEvent) {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
