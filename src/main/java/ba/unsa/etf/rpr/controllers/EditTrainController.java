541e0bf71167a83171d5907d9963574bd16ea2f1
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-13 20:42:20 +0100

    Added menuLinkOnAction method

--- src/main/java/ba/unsa/etf/rpr/controllers/EditTrainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EditTrainController.java
@@ -68,4 +68,17 @@ public class EditTrainController {
             e.getCause();
         }
     }
+    public void menuLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/adminpanelmenu.fxml")));
+            Stage stage = (Stage) ((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
 }
