e32ef75794f35184492bc358a906bba0f30d7309
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-13 20:35:50 +0100

    Added initialize method

--- src/main/java/ba/unsa/etf/rpr/controllers/EditTrainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EditTrainController.java
@@ -1,14 +1,21 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.TrainManager;
+import ba.unsa.etf.rpr.domain.Train;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EditTrainController {
     @FXML
     private ChoiceBox<Integer> routeBox;
+    @FXML
+    private List<Integer> trainIds = new ArrayList<Integer>();
     @FXML
     private TextField routeText;
     @FXML
@@ -21,4 +28,24 @@ public class EditTrainController {
     private Label message1;
     @FXML
     private Label message2;
+    TrainManager trainManager = new TrainManager();
+    private Integer[] hour = {5, 6, 7, 8, 9, 10};
+    private Integer[] min = {0, 10, 20, 30, 40, 50};
+
+    public void initialize() {
+        try {
+            List<Train> trains = trainManager.getAll();
+            for (int i = 0; i < trains.size(); i++) {
+                Train train = trains.get(i);
+                trainIds.add(train.getId());
+            }
+            routeBox.getItems().addAll(trainIds);
+            hourBox.getItems().addAll(hour);
+            minBox.getItems().addAll(min);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
 }
