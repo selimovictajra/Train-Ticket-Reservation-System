37735f577d6a22fa116fbdb8221f66a61120b7e0
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-13 15:15:44 +0100

    Added menuLinkOnAction method

--- src/main/java/ba/unsa/etf/rpr/controllers/DeleteTrainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/DeleteTrainController.java
@@ -48,6 +48,34 @@ public class DeleteTrainController {
         }
     }
 
+    public void homeLinkOnAction(javafx.event.ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/adminpanelhome.fxml")));
+            Stage stage = (Stage)((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception exception) {
+            exception.printStackTrace();
+            exception.getCause();
+        }
+    }
+
+    public void menuLinkOnAction(javafx.event.ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/adminpanelmenu.fxml")));
+            Stage stage = (Stage)((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception exception) {
+            exception.printStackTrace();
+            exception.getCause();
+        }
+    }
+
     public void logoutLinkOnAction(javafx.event.ActionEvent actionEvent) {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
