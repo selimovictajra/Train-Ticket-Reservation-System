5c6ca23884048258a4cde7f17963cab427be9d49
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-13 16:24:07 +0100

    Added initialize method

--- src/main/java/ba/unsa/etf/rpr/controllers/AddTrainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AddTrainController.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.TrainManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
@@ -12,9 +13,19 @@ public class AddTrainController {
     @FXML
     private DatePicker date;
     @FXML
-    private ChoiceBox<Integer> timeBox;
+    private ChoiceBox<Integer> hourBox;
+    @FXML
+    private ChoiceBox<Integer> minBox;
     @FXML
     private Label message1;
     @FXML
     private Label message2;
+    TrainManager trainManager = new TrainManager();
+    private Integer[] hour = {5, 6, 7, 8, 9, 10};
+    private Integer[] min = {0, 10, 20, 30, 40, 50};
+
+    public void initialize() {
+        hourBox.getItems().addAll(hour);
+        minBox.getItems().addAll(min);
+    }
 }
