7b2830622415e2482c94f86e96513c0fb7d44b97
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-13 16:31:24 +0100

    Added logoutLinkOnAction method

--- src/main/java/ba/unsa/etf/rpr/controllers/AddTrainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AddTrainController.java
@@ -2,10 +2,16 @@ package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.business.TrainManager;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class AddTrainController {
     @FXML
@@ -27,5 +33,19 @@ public class AddTrainController {
     public void initialize() {
         hourBox.getItems().addAll(hour);
         minBox.getItems().addAll(min);
+    }
+
+    public void logoutLinkOnAction(javafx.event.ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
+            Stage stage = (Stage)((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
     }
 }
