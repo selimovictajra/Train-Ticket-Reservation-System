cebec3dc020b7a8acf372e0fd335d519531fe4b2
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-13 17:42:26 +0100

    Added addTrainLinkOnAction method

--- src/main/java/ba/unsa/etf/rpr/controllers/DeleteTrainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/DeleteTrainController.java
@@ -77,6 +77,19 @@ public class DeleteTrainController {
             exception.getCause();
         }
     }
+    public void addTrainLinkOnAction(javafx.event.ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/addLink.fxml")));
+            Stage stage = (Stage)((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception exception) {
+            exception.printStackTrace();
+            exception.getCause();
+        }
+    }
 
     public void logoutLinkOnAction(javafx.event.ActionEvent actionEvent) {
         try {
