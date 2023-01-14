9fa6b3ff1ddbe8ac0d15f930097fe7fa0d4700ac
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-14 12:35:17 +0100

    Created Model class for sharing data between controllers

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/Model.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.domain.Train;
+import ba.unsa.etf.rpr.domain.User;
+
+public class Model {
+    private static Model instance;
+    private Train train;
+    private User user;
+
+    private Model() {}
+
+    public static Model getInstance() {
+        if (instance == null) {
+            instance = new Model();
+        }
+        return instance;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
