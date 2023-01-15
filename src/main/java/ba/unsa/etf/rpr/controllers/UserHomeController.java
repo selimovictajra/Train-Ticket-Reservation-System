9d104298351639728cf15d630f14cef773b80802
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-15 20:50:41 +0100

    Added logoutLinkOnAction method

--- src/main/java/ba/unsa/etf/rpr/controllers/UserHomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserHomeController.java
@@ -1,8 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.exceptions.TrainException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.awt.*;
+import java.util.Objects;
 
 public class UserHomeController {
     @FXML
@@ -11,5 +17,19 @@ public class UserHomeController {
     public void initialize() {
         Model model = Model.getInstance();
         userLabel.setText(model.getUser().getName());
+    }
+
+    public void logoutLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
+            Stage stage = (Stage) ((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
     }
 }
