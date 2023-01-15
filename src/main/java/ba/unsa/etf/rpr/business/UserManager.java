187f0b874ec1b0ea11480fce130dd4373a7faeff
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-15 13:28:34 +0100

    Small changes

--- src/main/java/ba/unsa/etf/rpr/business/UserManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -27,4 +27,19 @@ public class UserManager {
     public List<User> getAll() throws TrainException {
         return DaoFactory.userDao().getAll();
     }
+    public User add(User u) throws TrainException {
+        return DaoFactory.userDao().add(u);
+    }
+    public User getById(Integer id) throws TrainException {
+        return DaoFactory.userDao().getById(id);
+    }
+    public Integer checkUsernamePassword(String usernameTextField, String passwordField) throws TrainException {
+        return DaoFactory.userDao().checkUsernamePassword(usernameTextField,passwordField);
+    }
+    public boolean isRole(String usernameField) throws TrainException {
+        return DaoFactory.userDao().isRole(usernameField);
+    }
+    public boolean findUsername(String usernameField) throws TrainException {
+        return DaoFactory.userDao().findUsername(usernameField);
+    }
 }
