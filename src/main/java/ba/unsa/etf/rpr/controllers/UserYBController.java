b0815ccad457a41e53f9a2326a2edd3a8e5d1531
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-15 18:37:39 +0100

    Added initialize method

--- src/main/java/ba/unsa/etf/rpr/controllers/UserYBController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserYBController.java
@@ -1,14 +1,61 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.ReservationManager;
+import ba.unsa.etf.rpr.business.TrainManager;
+import ba.unsa.etf.rpr.business.UserManager;
+import ba.unsa.etf.rpr.dao.ReservationDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Reservation;
+import ba.unsa.etf.rpr.domain.Train;
 import ba.unsa.etf.rpr.exceptions.TrainException;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import javax.swing.text.TabableView;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class UserYBController {
+
+    private final TrainManager trainManager = new TrainManager();
+    private final UserManager userManager = new UserManager();
+    private final ReservationManager reservationManager = new ReservationManager();
+    @FXML
+    public TableView<Train> bookingTable;
+    @FXML
+    public TableColumn<Train, String> routeColumn;
+    @FXML
+    public TableColumn<Train, LocalDateTime> departureColumn;
+    @FXML
+    public TableColumn<Train, Integer> priceColumn;
+    @FXML
+    private Label userLabel;
+
+    public void initialize() {
+        try {
+            Model model = Model.getInstance();
+            userLabel.setText(model.getUser().getName());
+            ReservationDaoSQLImpl reservationDaoSQL = new ReservationDaoSQLImpl();
+            routeColumn.setCellValueFactory(new PropertyValueFactory<>("route"));
+            departureColumn.setCellValueFactory(new PropertyValueFactory<>("departure"));
+            bookingTable.setItems(FXCollections.observableList(reservationDaoSQL.getByUser(model.getUser().getId())));
+            bookingTable.refresh();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
 
 
     public void logoutLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
