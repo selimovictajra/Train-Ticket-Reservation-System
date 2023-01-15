642ea494699b665f13f10bc389d7106c4f5ae738
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-15 16:40:48 +0100

    Created UserYBController class

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserYBController.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.exceptions.TrainException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class UserYBController {
+
+
+    public void logoutLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
+            Stage stage = (Stage) ((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+}
