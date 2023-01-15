825abd1b90d4be813c2fee1b97e695630a10474e
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-15 13:28:47 +0100

    Small changes

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -3,9 +3,6 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.TrainException;
 
-import java.sql.ResultSet;
-import java.util.Map;
-
 /**
  * Dao interface for User domain bean
  *
@@ -13,7 +10,7 @@ import java.util.Map;
  */
 public interface UserDao extends Dao<User> {
 
-    boolean checkUsernamePassword(String usernameTextField, String passwordField) throws TrainException;
+    Integer checkUsernamePassword(String usernameTextField, String passwordField) throws TrainException;
     boolean findUsername(String usernameField) throws TrainException;
     boolean isRole(String usernameField) throws TrainException;
     int numberOfUsers() throws TrainException;
