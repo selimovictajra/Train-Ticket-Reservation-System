9aeef58d0c99388b2cbff1c35c388e5e13e2beef
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-15 20:52:35 +0100

    Added yBookingsLinkOnAction method

--- src/main/java/ba/unsa/etf/rpr/controllers/UserHomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserHomeController.java
@@ -45,4 +45,17 @@ public class UserHomeController {
             e.getCause();
         }
     }
+    public void yBookingsLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/userPanelYourBookings.fxml")));
+            Stage stage = (Stage) ((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
 }
