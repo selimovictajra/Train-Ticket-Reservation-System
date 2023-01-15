722044962b942513fd5875ef64f84bcfaff00bd2
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-15 13:28:54 +0100

    Small changes

--- src/main/java/ba/unsa/etf/rpr/controllers/UserBookingController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserBookingController.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.ReservationManager;
 import ba.unsa.etf.rpr.business.TrainManager;
+import ba.unsa.etf.rpr.domain.Reservation;
 import ba.unsa.etf.rpr.domain.Train;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.TrainException;
@@ -8,6 +10,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -17,19 +20,22 @@ public class UserBookingController {
     @FXML
     private ChoiceBox<String> routeBox;
     @FXML
-    private ChoiceBox<String> dateBox;
+    private ChoiceBox<LocalDate> dateBox;
     @FXML
-    private ChoiceBox<String> timeBox;
-    private List<String> routes = new ArrayList<String>();
-    private List<String> dateL = new ArrayList<String>();
-    private List<String> timeL = new ArrayList<String>();
+    private ChoiceBox<LocalTime> timeBox;
+    private ArrayList<String> routes = new ArrayList<String>();
+    private ArrayList<LocalDate> dateL = new ArrayList<LocalDate>();
+    private ArrayList<LocalTime> timeL = new ArrayList<LocalTime>();
     @FXML
     private Label userLabel;
     @FXML
     private Label message1;
     @FXML
     private Label message2;
+    @FXML
+    private Label priceLabel;
     TrainManager trainManager = new TrainManager();
+    ReservationManager reservationManager = new ReservationManager();
 
     public void initialize() {
         try {
@@ -50,8 +56,8 @@ public class UserBookingController {
                             //System.out.println("usao");
                             Train train = trains.get(i);
                             LocalDateTime localDateTime = train.getDeparture();
-                            dateL.add(localDateTime.toLocalDate().toString());
-                            timeL.add(localDateTime.toLocalTime().toString());
+                            dateL.add(localDateTime.toLocalDate());
+                            timeL.add(localDateTime.toLocalTime());
                         }
                     }
                     dateBox.getItems().addAll(dateL);
@@ -60,16 +66,49 @@ public class UserBookingController {
                 catch(Exception e) {
                     message1.setText(e.getMessage());
                     message2.setText("");
+                    priceLabel.setText("Price: 30$");
                 }
             });
         }
         catch (Exception e) {
             message1.setText(e.getMessage());
             message2.setText("");
+            priceLabel.setText("");
         }
     }
 
-    public void bookButtonOnAction(javafx.event.ActionEvent actionEvent) {
+    public void bookButtonOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
+        try {
+            reservationManager.validateBookFields(routeBox.toString(), dateBox.getValue(), timeBox.getValue());
+            Reservation reservation = new Reservation();
+            Model model = Model.getInstance();
+            User user = model.getUser();
+            //train building
+            Train train = new Train();
+            LocalDate localDate = dateBox.getValue();
+            LocalDateTime localDateTime = localDate.atTime(timeBox.getValue());
+            List<Train> trains = trainManager.getAll();
+            System.out.println("ok");
+            for (int i = 0; i < trains.size(); i++) {
+                Train train1 = trains.get(i);
+                if (train1.getRoute().equals(routeBox.getValue()) && train1.getDeparture().equals(localDateTime)) {
+                    train.setId(train1.getId());
+                    reservation.setTrain(train1);
+                    System.out.println("usao " + train1.getId());
+                }
+            }
+            //reservation building
+            reservation.setPrice(30);
+            reservation.setUser(user);
+            reservationManager.add(reservation);
+            message1.setText("");
+            message2.setText("You have been successfully booked train ticket!");
 
+        }
+        catch (TrainException e) {
+            message1.setText(e.getMessage());
+            System.out.println(e.getMessage());
+            message2.setText("");
+        }
     }
 }
