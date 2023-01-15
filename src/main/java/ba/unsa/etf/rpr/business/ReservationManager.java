c0c96cfb07c2585123e8d7c2621c65bbc9639aad
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-15 13:29:12 +0100

    Small changes

--- src/main/java/ba/unsa/etf/rpr/business/ReservationManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ReservationManager.java
@@ -4,6 +4,8 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Reservation;
 import ba.unsa.etf.rpr.exceptions.TrainException;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public class ReservationManager {
@@ -25,5 +27,14 @@ public class ReservationManager {
 
     public List<Reservation> getAll() throws TrainException {
         return DaoFactory.reservationDao().getAll();
+    }
+
+    public void validateBookFields(String route, LocalDate date, LocalTime time) throws TrainException {
+        if(route.isEmpty() || date == null || time == null) {
+            throw new TrainException("All fields must be filled in!");
+        }
+    }
+    public Reservation add(Reservation r) throws TrainException {
+        return DaoFactory.reservationDao().add(r);
     }
 }
