21af246d27694e61ad99c38653e6d2ab83bf8f31
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-15 13:37:50 +0100

    Added logoutLinkOnAction method

--- src/main/java/ba/unsa/etf/rpr/controllers/UserBookingController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserBookingController.java
@@ -7,14 +7,19 @@ import ba.unsa.etf.rpr.domain.Train;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.TrainException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class UserBookingController {
     @FXML
@@ -57,16 +62,36 @@ public class UserBookingController {
                             Train train = trains.get(i);
                             LocalDateTime localDateTime = train.getDeparture();
                             dateL.add(localDateTime.toLocalDate());
-                            timeL.add(localDateTime.toLocalTime());
+                            //timeL.add(localDateTime.toLocalTime());
                         }
                     }
                     dateBox.getItems().addAll(dateL);
+                    //timeBox.getItems().addAll(timeL);
+                }
+                catch(Exception e) {
+                    message1.setText(e.getMessage());
+                    message2.setText("");
+                    priceLabel.setText("");
+                }
+            });
+            dateBox.getSelectionModel().selectedItemProperty().addListener((observable, oldvalue, newvalue) -> {
+                try {
+                    for (int i = 0; i < trains.size(); i++) {
+                        if (trains.get(i).getRoute().equals(routeBox.getValue())) {
+                            //System.out.println("usao");
+                            Train train = trains.get(i);
+                            LocalDateTime localDateTime = train.getDeparture();
+                            //dateL.add(localDateTime.toLocalDate());
+                            timeL.add(localDateTime.toLocalTime());
+                        }
+                    }
+                    //dateBox.getItems().addAll(dateL);
                     timeBox.getItems().addAll(timeL);
                 }
                 catch(Exception e) {
                     message1.setText(e.getMessage());
                     message2.setText("");
-                    priceLabel.setText("Price: 30$");
+                    priceLabel.setText("");
                 }
             });
         }
@@ -103,12 +128,27 @@ public class UserBookingController {
             reservationManager.add(reservation);
             message1.setText("");
             message2.setText("You have been successfully booked train ticket!");
-
+            priceLabel.setText("Price: 30$");
         }
         catch (TrainException e) {
             message1.setText(e.getMessage());
             System.out.println(e.getMessage());
             message2.setText("");
+            priceLabel.setText("");
+        }
+    }
+
+    public void logoutLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
+            Stage stage = (Stage) ((javafx.scene.Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
         }
     }
 }
