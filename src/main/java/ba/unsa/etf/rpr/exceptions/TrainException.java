947a4c6acbd42e275d843fd6ac36ea34bb12a045
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-18 21:47:21 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/exceptions/TrainException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/TrainException.java
@@ -1,5 +1,10 @@
 package ba.unsa.etf.rpr.exceptions;
 
+/**
+ * TrainException is a custom exception class for handling errors related to train operations.
+ * It extends the base Exception class and provides two constructors for creating instances of the exception.
+ * @author Tajra Selimovic
+ */
 public class TrainException extends Exception{
     public TrainException(String message) {
         super(message);
