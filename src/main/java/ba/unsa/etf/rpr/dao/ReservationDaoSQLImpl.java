eaf8db9cad7d2916da8c4e9db6446f64c73b1a08
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-19 22:40:59 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/ReservationDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ReservationDaoSQLImpl.java
@@ -8,11 +8,23 @@ import java.io.FileReader;
 import java.sql.*;
 import java.util.*;
 
+/**
+ * MySQL implementation of the DAO
+ * @author Tajra Selimovic
+ */
 public class ReservationDaoSQLImpl extends AbstractDao<Reservation> implements ReservationDao{
+    /**
+     * Private constructor for the ReservationDaoSQLImpl class.
+     */
     public ReservationDaoSQLImpl(){
         super("Reservations");
     }
-
+    /**
+     * Maps a row from the result set to a Reservation object
+     * @param rs The result set from the database query
+     * @return A Reservation object with properties set according to the values in the result set
+     * @throws TrainException if there is an error when retrieving values from the result set
+     */
     @Override
     public Reservation row2object(ResultSet rs) throws TrainException {
         try {
@@ -25,7 +37,11 @@ public class ReservationDaoSQLImpl extends AbstractDao<Reservation> implements R
             throw new TrainException(e.getMessage(), e);
         }
     }
-
+    /**
+     * Takes in a Reservation object and converts it into a Map of String keys and Object values.
+     * @param object - a bean object for specific table
+     * @return A Map<String, Object> containing the fields of the Reservation object as key-value pairs.
+     */
     @Override
     public Map<String, Object> object2row(Reservation object) {
         Map<String, Object> item = new TreeMap<String, Object>();
@@ -34,6 +50,12 @@ public class ReservationDaoSQLImpl extends AbstractDao<Reservation> implements R
         item.put("train_id", object.getTrain().getId());
         return item;
     }
+    /**
+     * Fetches all reservations from database with given idUser
+     * @param idUser int
+     * @return List of Trains
+     * @throws TrainException in case of problems with database
+     */
     @Override
     public List<Train> getByUser(int idUser) throws TrainException {
         try {
