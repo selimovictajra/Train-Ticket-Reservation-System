a91f059434481d2fff187e2c90541dce70ef4b77
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-19 22:41:11 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -9,10 +9,33 @@ import ba.unsa.etf.rpr.exceptions.TrainException;
  * @author Tajra Selimovic
  */
 public interface UserDao extends Dao<User> {
-
+    /**
+     * Checks if username and password are correct
+     * @param usernameTextField String
+     * @param passwordField String
+     * @return Integer
+     * @throws TrainException in case of problems with database
+     */
     Integer checkUsernamePassword(String usernameTextField, String passwordField) throws TrainException;
+    /**
+     * Checks if username already exists
+     * @param usernameField String
+     * @return boolean
+     * @throws TrainException in case of problems with database
+     */
     boolean findUsername(String usernameField) throws TrainException;
+    /**
+     * Checks if user is admin or not
+     * @param usernameField String
+     * @return boolean
+     * @throws TrainException in case of problems with database
+     */
     boolean isRole(String usernameField) throws TrainException;
+    /**
+     * Returns the number of users from database
+     * @return Integer
+     * @throws TrainException in case of problems
+     */
     int numberOfUsers() throws TrainException;
 
 }
