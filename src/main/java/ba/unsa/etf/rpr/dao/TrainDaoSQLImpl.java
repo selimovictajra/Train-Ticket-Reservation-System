c50013862b716d06378947cb319fe4fdcffdd949
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-19 22:41:06 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/TrainDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/TrainDaoSQLImpl.java
@@ -8,11 +8,24 @@ import java.io.FileReader;
 import java.sql.*;
 import java.util.*;
 
+/**
+ * MySQL implementation of the DAO
+ * @author Tajra Selimovic
+ */
+
 public class TrainDaoSQLImpl extends AbstractDao<Train> implements TrainDao{
+    /**
+     * Private constructor for the TrainDaoSQLImpl class.
+     */
     public TrainDaoSQLImpl(){
         super("Trains");
     }
-
+    /**
+     * Maps a row from the result set to a Train object
+     * @param rs The result set from the database query
+     * @return A Train object with properties set according to the values in the result set
+     * @throws TrainException if there is an error when retrieving values from the result set
+     */
     @Override
     public Train row2object(ResultSet rs) throws TrainException {
         try {
@@ -27,7 +40,11 @@ public class TrainDaoSQLImpl extends AbstractDao<Train> implements TrainDao{
             throw new TrainException(e.getMessage(), e);
         }
     }
-
+    /**
+     * Takes in a Train object and converts it into a Map of String keys and Object values.
+     * @param object - a bean object for specific table
+     * @return A Map<String, Object> containing the fields of the Train object as key-value pairs.
+     */
     @Override
     public Map<String, Object> object2row(Train object) {
         Map<String, Object> item = new TreeMap<>();
