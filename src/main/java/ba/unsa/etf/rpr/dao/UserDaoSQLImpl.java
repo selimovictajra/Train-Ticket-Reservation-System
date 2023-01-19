bc760260b7c24266aa7bdd182acaebf30a1763b0
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-19 22:41:19 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
@@ -7,10 +7,23 @@ import ba.unsa.etf.rpr.exceptions.TrainException;
 import java.sql.*;
 import java.util.*;
 
+/**
+ * MySQL implementation of the DAO
+ * @author Tajra Selimovic
+ */
 public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
+    /**
+     * Private constructor for the UserDaoSQLImpl class.
+     */
     public UserDaoSQLImpl(){
         super("Users");
     }
+    /**
+     * Maps a row from the result set to a User object
+     * @param rs The result set from the database query
+     * @return A User object with properties set according to the values in the result set
+     * @throws TrainException if there is an error when retrieving values from the result set
+     */
     @Override
     public User row2object(ResultSet rs) throws TrainException {
         try {
@@ -26,6 +39,11 @@ public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
         }
     }
 
+    /**
+     * Takes in a User object and converts it into a Map of String keys and Object values.
+     * @param object - a bean object for specific table
+     * @return A Map<String, Object> containing the fields of the User object as key-value pairs.
+     */
     @Override
     public Map<String, Object> object2row(User object) {
         Map<String, Object> item = new TreeMap<String, Object>();
@@ -36,7 +54,12 @@ public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
         item.put("password", object.getPassword());
         return item;
     }
-
+    /**
+     * Checks if username already exists
+     * @param usernameField String
+     * @return boolean
+     * @throws TrainException in case of problems with database
+     */
     @Override
     public boolean findUsername(String usernameField) throws TrainException {
         String insert = "SELECT count(username) from Users where username='" + usernameField + "'";
@@ -52,6 +75,12 @@ public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
         }
         return false;
     }
+    /**
+     * Checks if user is admin or not
+     * @param usernameField String
+     * @return boolean
+     * @throws TrainException in case of problems with database
+     */
     @Override
     public boolean isRole(String usernameField) throws TrainException {
         String insert = "SELECT username from Users where role = 1";
@@ -67,7 +96,13 @@ public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
         }
         return false;
     }
-
+    /**
+     * Checks if username and password are correct
+     * @param usernameTextField String
+     * @param passwordField String
+     * @return Integer
+     * @throws TrainException in case of problems with database
+     */
     @Override
     public Integer checkUsernamePassword(String usernameTextField, String passwordField) throws TrainException{
         String insert = "SELECT id from Users where username ='" + usernameTextField + "' AND password='"
@@ -85,6 +120,11 @@ public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
         return null;
     }
 
+    /**
+     * Returns the number of users from database
+     * @return Integer
+     * @throws TrainException in case of problems
+     */
     @Override
     public int numberOfUsers() throws TrainException {
         String insert = "SELECT count(1) FROM Users";
