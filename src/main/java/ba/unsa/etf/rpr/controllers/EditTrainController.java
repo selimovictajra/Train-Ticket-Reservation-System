09a455b8a36185cd29563f06cb874cf0f85fb552
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-20 22:55:41 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/controllers/EditTrainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EditTrainController.java
@@ -20,11 +20,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * This class is used to handle the editing of a train's information in the admin panel.
+ * @author Tajra Selimovic
+ */
 public class EditTrainController {
     @FXML
     private ChoiceBox<Integer> routeBox;
     @FXML
-    private List<Integer> trainIds = new ArrayList<Integer>();
+    private final List<Integer> trainIds = new ArrayList<Integer>();
     @FXML
     private TextField routeText;
     @FXML
@@ -40,15 +44,18 @@ public class EditTrainController {
     @FXML
     private Label message2;
     TrainManager trainManager = new TrainManager();
-    private Integer[] hour = {5, 6, 7, 8, 9, 10};
-    private Integer[] min = {0, 10, 20, 30, 40, 50};
-    private Integer[] prices = {10, 15, 20, 25, 30};
+    private final Integer[] hour = {5, 6, 7, 8, 9, 10};
+    private final Integer[] min = {0, 10, 20, 30, 40, 50};
+    private final Integer[] prices = {10, 15, 20, 25, 30};
 
+    /**
+     * Initializes the fields of the Edit Train Route view.
+     * Populates the fields with the data of the selected train route from the database.
+     */
     public void initialize() {
         try {
             List<Train> trains = trainManager.getAll();
-            for (int i = 0; i < trains.size(); i++) {
-                Train train = trains.get(i);
+            for (Train train : trains) {
                 trainIds.add(train.getId());
             }
             routeBox.getItems().addAll(trainIds);
@@ -77,7 +84,12 @@ public class EditTrainController {
             e.getCause();
         }
     }
-
+    /**
+     * Handles the action when the "Edit" button is clicked.
+     * Validates the fields for editing a train route and updates the train route in the database.
+     * @param actionEvent the action event that triggers the method
+     * @throws TrainException if the train route cannot be edited
+     */
     public void editButtonOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
         try {
             trainManager.validateDeleteFields(routeBox.getValue());
@@ -99,7 +111,12 @@ public class EditTrainController {
             message2.setText("");
         }
     }
-
+    /**
+     * The homeTrainLinkOnAction() method is called when the user clicks the "Home" link.
+     * Switches the user to the home window.
+     * If any exception is thrown during the execution, it will be caught and the error message will be printed to the console.
+     * @param actionEvent the event that triggers the action
+     */
     public void homeLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/adminpanelhome.fxml")));
@@ -113,6 +130,12 @@ public class EditTrainController {
             e.getCause();
         }
     }
+    /**
+     * The menuTrainLinkOnAction() method is called when the user clicks the "Menu" link.
+     * Switches the user to the menu window.
+     * If any exception is thrown during the execution, it will be caught and the error message will be printed to the console.
+     * @param actionEvent the event that triggers the action
+     */
     public void menuLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/adminpanelmenu.fxml")));
@@ -126,6 +149,12 @@ public class EditTrainController {
             e.getCause();
         }
     }
+    /**
+     * The deleteTrainLinkOnAction() method is called when the user clicks the "Delete" link.
+     * Switches the user to the delete Train window.
+     * If any exception is thrown during the execution, it will be caught and the error message will be printed to the console.
+     * @param actionEvent the event that triggers the action
+     */
     public void deleteTrainLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/deleteLink.fxml")));
@@ -139,6 +168,12 @@ public class EditTrainController {
             e.getCause();
         }
     }
+    /**
+     * The addTrainLinkOnAction() method is called when the user clicks the "Add" link.
+     * Switches the user to the add Train window.
+     * If any exception is thrown during the execution, it will be caught and the error message will be printed to the console.
+     * @param actionEvent the event that triggers the action
+     */
     public void addTrainLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/addLink.fxml")));
@@ -152,6 +187,12 @@ public class EditTrainController {
             e.getCause();
         }
     }
+    /**
+     * The logoutLinkOnAction() method is called when the user clicks the "Logout" link.
+     * Switches the user to the login window.
+     * If any exception is thrown during the execution, it will be caught and the error message will be printed to the console.
+     * @param actionEvent the event that triggers the action
+     */
     public void logoutLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
