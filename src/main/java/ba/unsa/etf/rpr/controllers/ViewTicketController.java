ce8dabfd52f0f3b1717495e2758e9cb5dc39aef9
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-20 22:56:47 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/controllers/ViewTicketController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ViewTicketController.java
@@ -5,6 +5,12 @@ import javafx.fxml.FXML;
 import java.time.LocalDateTime;
 import javafx.scene.control.Label;
 
+/**
+ * This class is a JavaFX controller for the user's "View Ticket" screen.
+ * It is responsible for displaying the details of a specific train ticket reservation,
+ * including the train id, departure time, passenger name, date, and ticket number.
+ * @author Tajra Selimovic
+ */
 public class ViewTicketController {
     @FXML
     public Label trainIdLabel;
@@ -27,6 +33,10 @@ public class ViewTicketController {
     @FXML
     public Label ticketLabel1;
 
+    /**
+     * It sets the text of various labels to the values of properties of Train and Reservation objects retrieved from the Model class,
+     * and a User object also retrieved from the Model class.
+     */
     public void initialize() {
         Model model = Model.getInstance();
         int i = model.getTrain().getId();
