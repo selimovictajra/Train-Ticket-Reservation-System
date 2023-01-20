a0d161e57a29b655383ac42fcece59b747b7d779
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-20 22:56:27 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/controllers/UserHomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserHomeController.java
@@ -10,15 +10,28 @@ import javafx.scene.control.Label;
 
 import java.util.Objects;
 
+/**
+ * This class is a JavaFX controller for the user home screen.
+ * It contains methods for handling button events for the logout, bookings, and your bookings links.
+ * It also has a method for initializing the user's name on the home screen.
+ * @author Tajra Selimovic
+ */
 public class UserHomeController {
     @FXML
     private Label userLabel;
-
+    /**
+     * Initializes the user's name on the home screen by getting the current user from the Model class.
+     */
     public void initialize() {
         Model model = Model.getInstance();
         userLabel.setText(model.getUser().getName());
     }
-
+    /**
+     * Handles the action for the logout link.
+     * Navigates to the login page.
+     * @param actionEvent the action event
+     * @throws TrainException in case of problems
+     */
     public void logoutLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
@@ -32,6 +45,12 @@ public class UserHomeController {
             e.getCause();
         }
     }
+    /**
+     * Handles the action for the booking link.
+     * Navigates to the Booking page.
+     * @param actionEvent the action event
+     * @throws TrainException in case of problems
+     */
     public void bookingLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/userPanelBooking.fxml")));
@@ -45,6 +64,12 @@ public class UserHomeController {
             e.getCause();
         }
     }
+    /**
+     * Handles the action for the YourBookings link.
+     * Navigates to the YourBookings page.
+     * @param actionEvent the action event
+     * @throws TrainException in case of problems
+     */
     public void yBookingsLinkOnAction(javafx.event.ActionEvent actionEvent) throws TrainException {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/userPanelYourBookings.fxml")));
