bced63eba5f22ba3d8f0044b8051948412513c49
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-01-20 22:54:11 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelHomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelHomeController.java
@@ -1,6 +1,6 @@
 package ba.unsa.etf.rpr.controllers;
 
-import ba.unsa.etf.rpr.dao.UserDaoSQLImpl;
+import ba.unsa.etf.rpr.business.UserManager;
 import ba.unsa.etf.rpr.exceptions.TrainException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,14 +13,26 @@ import java.awt.event.ActionEvent;
 import java.sql.SQLException;
 import java.util.Objects;
 
+/**
+ * The AdminPanelHomeController class is responsible for the functionality of the admin panel home page.
+ *
+ * @author Tajra Selimovic
+ */
+
 public class AdminPanelHomeController {
+    UserManager userManager = new UserManager();
+    @FXML
     public javafx.scene.control.Label usersLabel;
+    @FXML
     public javafx.scene.control.Label adminLabel;
 
+    /**
+     * The initialize() method is called when the admin panel home page is loaded.
+     * It retrieves the name of the currently logged in admin user and number of users and sets it on page.
+     */
     public void initialize() {
-        UserDaoSQLImpl userDaoSQL = new UserDaoSQLImpl();
         try {
-            int num = userDaoSQL.numberOfUsers();
+            int num = userManager.numberOfUsers();
             usersLabel.setText(String.valueOf(num));
             Model model = Model.getInstance();
             adminLabel.setText(model.getUser().getName());
@@ -29,7 +41,12 @@ public class AdminPanelHomeController {
             e.printStackTrace();
         }
     }
-
+    /**
+     * The menuTrainLinkOnAction() method is called when the user clicks the "Menu" link.
+     * Switches the user to the menu window.
+     * If any exception is thrown during the execution, it will be caught and the error message will be printed to the console.
+     * @param actionEvent the event that triggers the action
+     */
     public void menuLinkOnAction(javafx.event.ActionEvent actionEvent) {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/adminpanelmenu.fxml")));
@@ -43,7 +60,12 @@ public class AdminPanelHomeController {
             exception.getCause();
         }
     }
-
+    /**
+     * The deleteTrainLinkOnAction() method is called when the user clicks the "Delete" link.
+     * Switches the user to the delete Train window.
+     * If any exception is thrown during the execution, it will be caught and the error message will be printed to the console.
+     * @param actionEvent the event that triggers the action
+     */
     public void deleteTrainLinkOnAction(javafx.event.ActionEvent actionEvent) {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/deleteLink.fxml")));
@@ -57,6 +79,12 @@ public class AdminPanelHomeController {
             exception.getCause();
         }
     }
+    /**
+     * The addTrainLinkOnAction() method is called when the user clicks the "Add" link.
+     * Switches the user to the add Train window.
+     * If any exception is thrown during the execution, it will be caught and the error message will be printed to the console.
+     * @param actionEvent the event that triggers the action
+     */
     public void addTrainLinkOnAction(javafx.event.ActionEvent actionEvent) {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/addLink.fxml")));
@@ -70,6 +98,12 @@ public class AdminPanelHomeController {
             exception.getCause();
         }
     }
+    /**
+     * The editTrainLinkOnAction() method is called when the user clicks the "Edit" link.
+     * Switches the user to the edit Train window.
+     * If any exception is thrown during the execution, it will be caught and the error message will be printed to the console.
+     * @param actionEvent the event that triggers the action
+     */
     public void editTrainLinkOnAction(javafx.event.ActionEvent actionEvent) {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/editLink.fxml")));
@@ -83,6 +117,12 @@ public class AdminPanelHomeController {
             exception.getCause();
         }
     }
+    /**
+     * The logoutLinkOnAction() method is called when the user clicks the "Logout" link.
+     * Switches the user to the login window.
+     * If any exception is thrown during the execution, it will be caught and the error message will be printed to the console.
+     * @param actionEvent the event that triggers the action
+     */
     public void logoutLinkOnAction(javafx.event.ActionEvent actionEvent) {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
