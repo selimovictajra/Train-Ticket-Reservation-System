ead945ad51c038a495ad6118fddd3c357715f8cd
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-02-12 16:12:50 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/exceptions/TrainException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/TrainException.java
@@ -6,10 +6,19 @@ package ba.unsa.etf.rpr.exceptions;
  * @author Tajra Selimovic
  */
 public class TrainException extends Exception{
+    /**
+     * Constructs a new TrainException with the specified detail message.
+     * @param message the detail message.
+     */
     public TrainException(String message) {
         super(message);
     }
 
+    /**
+     * nstructs a new TrainException with the specified detail message and cause.
+     * @param message the detail message.
+     * @param cause the cause
+     */
     public TrainException(String message, Throwable cause) {
         super(message, cause);
     }
