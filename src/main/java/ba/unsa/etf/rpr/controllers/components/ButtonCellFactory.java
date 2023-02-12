db66049902c45dfc51c9b048f51be13dd51eae14
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-02-12 16:11:44 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/controllers/components/ButtonCellFactory.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/components/ButtonCellFactory.java
@@ -8,21 +8,28 @@ import javafx.util.Callback;
 
 /**
  * Button cell factory for creation of buttons for each cell in the table
- * @param <T>
+ * @param <T> The type of the elements contained within the table column.
  */
 public class ButtonCellFactory<T> implements Callback<TableColumn<T, T>, TableCell<T, T>> {
 
     private final EventHandler<ActionEvent> buttonOne;
+
     /**
-     *
-     * @param buttonOne - event handler button (VIEW)
+     * Creates a new instance of the `ButtonCellFactory` class.
+     * @param buttonOne The event handler for the button (VIEW).
      */
     public ButtonCellFactory(EventHandler<ActionEvent> buttonOne){
         this.buttonOne = buttonOne;
     }
 
+    /**
+     * Returns a new instance of the `ButtonTableCell` class.
+     * @param quoteObjectTableColumn The table column.
+     * @return A new instance of the `ButtonTableCell` class.
+     */
     @Override
     public TableCell<T, T> call(TableColumn<T, T> quoteObjectTableColumn) {
         return new ButtonTableCell<>(buttonOne);
     }
 }
+
