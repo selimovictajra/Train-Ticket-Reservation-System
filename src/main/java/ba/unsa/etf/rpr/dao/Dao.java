eedd42050338848f1308337eaae0971dc742f70b
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-02-12 16:11:39 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 /**
  * Root interface for all DAO classes
- *
  * @author Tajra Selimovic
+ * @param <T> the type of entity that this DAO operates on
  */
 public interface Dao<T> {
 
@@ -15,6 +15,7 @@ public interface Dao<T> {
      * get entity from database base on ID
      * @param id primary key of entity
      * @return Entity from database
+     * @throws TrainException in case of problems
      */
     T getById(int id) throws TrainException;
 
@@ -22,6 +23,7 @@ public interface Dao<T> {
      * Saves entity into database
      * @param item bean for saving to database
      * @return saved item with id field populated
+     * @throws TrainException in case of problems
      */
     T add(T item) throws TrainException;
 
@@ -29,18 +31,21 @@ public interface Dao<T> {
      * Fully updates entity in database based on id (primary) match.
      * @param item - bean to be updated. id must be populated
      * @return updated version of bean
+     * @throws TrainException in case of problems
      */
     T update(T item) throws TrainException;
 
     /**
      * Hard delete of item from database with given id
      * @param id - primary key of entity
+     * @throws TrainException in case of problems
      */
     void delete(int id) throws TrainException;
 
     /**
      * Lists all entities from database. WARNING: Very slow operation because it reads all records.
      * @return List of entities from database
+     * @throws TrainException in case of problems
      */
     List<T> getAll() throws TrainException;
 }
