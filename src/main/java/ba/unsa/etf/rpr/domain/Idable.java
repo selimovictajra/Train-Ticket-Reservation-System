9ad304b01f673ab2c71c7e6e15524fd0c099c940
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-02-12 16:12:01 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/domain/Idable.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Idable.java
@@ -4,8 +4,15 @@ package ba.unsa.etf.rpr.domain;
  * Interface that forces all POJO beans to have ID field. Name is stupid but per standard
  */
 public interface Idable {
-
+    /**
+     * Setter for Id
+     * @param id int
+     */
     void setId(int id);
 
+    /**
+     * Getter for Id
+     * @return int
+     */
     int getId();
 }
