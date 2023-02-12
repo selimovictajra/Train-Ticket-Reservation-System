c34a99fca3192dcfdbf1e6053ad9f0e916d30683
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-02-12 16:12:34 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/domain/Train.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Train.java
@@ -16,46 +16,95 @@ public class Train implements Idable {
     private int capacity;
     private int price;
 
+    /**
+     * Default constructor
+     */
+    public Train() {}
+
+    /**
+     * Getter for Id
+     * @return int
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Setter for id
+     * @param train_id int
+     */
     public void setId(int train_id) {
         this.id = train_id;
     }
 
+    /**
+     * Getter for route
+     * @return String
+     */
     public String getRoute() {
         return route;
     }
 
+    /**
+     * Setter for route
+     * @param route String
+     */
     public void setRoute(String route) {
         this.route = route;
     }
 
+    /**
+     * Getter for departure
+     * @return LocalDateTime
+     */
     public LocalDateTime getDeparture() {
         return departure;
     }
 
+    /**
+     * Setter for departure
+     * @param departure LocalDateTime
+     */
     public void setDeparture(LocalDateTime departure) {
         this.departure = departure;
     }
 
+    /**
+     * Getter for capacity
+     * @return int
+     */
     public int getCapacity() {
         return capacity;
     }
 
+    /**
+     * Setter for capacity
+     * @param capacity int
+     */
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
 
+    /**
+     * Getter for price
+     * @return int
+     */
     public int getPrice() {
         return price;
     }
 
+    /**
+     * Setter for price
+     * @param price int
+     */
     public void setPrice(int price) {
         this.price = price;
     }
 
+    /**
+     * Converts Train to String
+     * @return String
+     */
     @Override
     public String toString() {
         return "Trains{" +
