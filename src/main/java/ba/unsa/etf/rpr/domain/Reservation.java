e52681c431e1f53dd5a583033f18b1d48ea0bb90
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-02-12 16:12:18 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/domain/Reservation.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Reservation.java
@@ -11,26 +11,51 @@ public class Reservation implements Idable {
     private User user;
     private Train train;
 
+    /**
+     * Default constructor
+     */
+    public Reservation() {}
+
+    /**
+     * Getter for Id
+     * @return int
+     */
     public int getId() {
         return id;
     }
-
+    /**
+     * Setter for Id
+     * @param reservation_id int
+     */
     public void setId(int reservation_id) {
         this.id = reservation_id;
     }
-
+    /**
+     * Getter for user
+     * @return User
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Setter for User
+     * @param u User
+     */
     public void setUser(User u) {
         this.user = u;
     }
-
+    /**
+     * Getter for train
+     * @return Train
+     */
     public Train getTrain() {
         return train;
     }
-
+    /**
+     * Setter for Train
+     * @param t Train
+     */
     public void setTrain(Train t) {
         this.train = t;
     }
