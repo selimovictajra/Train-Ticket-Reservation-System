d7046c53072ad7080e1a122eed2157f606fe3255
tselimovic2 <tselimovic2@etf.unsa.ba>
2023-02-12 16:13:00 +0100

    Added javadoc

--- src/main/java/ba/unsa/etf/rpr/domain/User.java
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -13,42 +13,87 @@ public class User implements Idable {
     private String username;
     private String password;
 
+    /**
+     * Default constructor
+     */
+    public User() {}
+
+    /**
+     * Getter for id
+     * @return int
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Setter for id
+     * @param user_id int
+     */
     public void setId(int user_id) {
         this.id = user_id;
     }
 
+    /**
+     * Getter for name
+     * @return String
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter for name
+     * @param name String
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Getter for role
+     * @return boolean
+     */
     public boolean isRole() {
         return role;
     }
 
+    /**
+     * Setter for role
+     * @param role boolean
+     */
     public void setRole(boolean role) {
         this.role = role;
     }
 
+    /**
+     * Getter for username
+     * @return String
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Setter for username
+     * @param username boolean
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Getter for password
+     * @return String
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Setter for password
+     * @param password String
+     */
     public void setPassword(String password) {
         this.password = password;
     }
